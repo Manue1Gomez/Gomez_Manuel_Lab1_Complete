882c3116fd2c6b25a7767890bcdf848fe02bf351
Manue1Gomez <24-16819@g.batstate-u.edu.ph>
2025-03-14 12:09:34 -0300

    Update Gomez_Manuel_Lab_2.cpp

--- Gomez_Manuel_Lab_2.cpp
+++ Gomez_Manuel_Lab_2.cpp
@@ -36,7 +36,7 @@ void traverse(Node *head){
         if(head == NULL){
             Node *newNode = createNode(data);
             head = newNode;
-            cout << " A new node has been inserted at the end \n" <<endl;
+            cout << "A new node has been inserted at the end \n" <<endl;
             return head;
         }
         Node *temp = new Node;
@@ -48,7 +48,7 @@ void traverse(Node *head){
         Node *newNode = createNode(data);
         temp->link = newNode;
         
-        cout << "A new node has been iniserted at the end \n" <<endl;
+        cout << "A new node has been inserted at the end \n" <<endl;
         return head;
     }
     
@@ -57,13 +57,13 @@ void traverse(Node *head){
         newNode->link = head;
         
         head = newNode;
-        cout<< " A new node has been inserted at the beginning \n" <<endl;
-        
+        cout<< "A new node has been inserted at the beginning \n" <<endl;
         return head;
     }
     
     string insertAfter(string after,string data,Node*head){
          Node *temp = new Node;
+         temp = head;
          
          while(temp->songName.compare(after)!= 0){
              if(temp == NULL){
@@ -75,38 +75,148 @@ void traverse(Node *head){
     newNode->link = temp->link;
     temp->link = newNode;
     
-    return "A new code has been added" + after + "\n";
+    return "A new code has been added after " + after + "\n" ;
+    cout <<endl;
+    }
+    
+    string deleteAtEnd(Node *head){
+        if(head == NULL){
+            return "The linked list is empty \n";
+        }
+        
+        if(head->link ==NULL){
+            delete head;
+            return "The head has been deleted \n";
+        }
+        
+        Node *temp = new Node;
+        temp = head;
+        
+        while(temp->link->link != NULL){
+            temp = temp->link;
+        }
+        
+        temp->link = NULL;
+        
+        return "A node has been deleted at the end \n";
+    }
+    
+    Node *deleteFromBeginning(Node *head){
+        if(head == NULL){
+            cout << "The link list is empty \n" <<endl;
+            return NULL;
+        }
+        
+        if(head->link == NULL){
+            delete head;
+        }
+        
+        head = head->link;
+        
+        cout <<"A code has been delete from the beginning \n"<<endl;
+        
+        return head;
+    }
+    
+    Node *deleteFromGivenNode(string givenNode, Node *head){
+        if(head == NULL){
+            cout << "The link list is empty. \n"<<endl;
+            return NULL;
+        }
+        
+        if(head->songName.compare(givenNode) ==0){
+            head = deleteFromBeginning(head);
+            cout << "The Node" + givenNode + "has been deleted.\n"<<endl;
+            return head;
+        }
+        
+        Node *temp = new Node;
+        Node *next = new Node;
+        temp = head;
+        next = temp->link;
+        
+        while(next->songName.compare(givenNode) !=0){
+            if(temp == NULL){
+                cout << "No such node exist. \n"<<endl;
+                return head;
+            }
+            next = next->link;
+            temp = temp->link;
+        }
+        
+        temp->link = next->link;
+        cout << "The Node " + givenNode + " has been deleted. \n"<<endl ;
+        return head;
     }
     
     int main(){
     
-    Node *sample = new Node;
-    sample->songName = "Heaven Knows by Orange and Lemon";
+    Node *head = createNode("Sanctuary by Joji");
+    traverse(head);
+    cout <<endl;
     
-    Node *head = createNode("sanctuary by Joji");
-    
+   
     head = insertAtEnd("Sunday Morning by Maroon 5", head);
+    traverse(head);
+    cout <<endl;
     
     head = insertAtEnd("Blinding Lights by The Weeknd", head);
+    traverse(head);
+    cout <<endl;
+    
     head = insertAtEnd("Shape Of You by Ed Sheeran", head);
+    traverse(head);
+    cout <<endl;
+    
     head = insertAtEnd("Someone you loved by Lewis Capaldi", head);
+    traverse(head);
+    cout <<endl;
+    
     
     head = insertAtbeginning("Die with a Smile by Lady Gaga and Bruno Mars", head);
-    head = insertAtbeginning("DtMF by Bad Bunny", head);
-    head = insertAtbeginning("Birds Of A Feather by Billie Eillish", head);
-    head = insertAtbeginning("Not like us by kendrick lamar", head);
-    
-    head = insertAfter  "Die with a Smile by Lady Gaga and Bruno Mars",("APT by Rose");
-    
-    
-    cout << sample->songName <<endl;
     traverse(head);
-    return 0;
+    cout <<endl;
+    
+    head = insertAtbeginning("DTMF by Bad Bunny", head);
+    traverse(head);
+    cout <<endl;
+    
+    head = insertAtbeginning("Birds Of A Feather by Billie Eillish", head);
+    traverse(head);
+    cout <<endl;
+    
+    head = insertAtbeginning("Not like us by kendrick lamar", head);
+    traverse(head);
+    cout <<endl;
+    
+    
+    cout << insertAfter ("Die with a Smile by Lady Gaga and Bruno Mars","APT by Rose", head)<<endl;
+    traverse(head);
+    cout <<endl;
+    
+    cout << insertAfter ("DTMF by Bad Bunny","Blue by Yung Kai", head)<<endl;
+    traverse(head);
+    cout <<endl;
+    
+    cout << insertAfter ("Birds Of A Feather by Billie Eillish","We Could Happen by AJ Rafael", head)<<endl;
+    traverse(head);
+    cout <<endl;
+   	
+	cout << deleteAtEnd(head);
+	traverse(head);
+	cout <<endl;
+	
+	head = deleteFromBeginning(head);
+	traverse(head);
+	cout <<endl;
+
+	head = deleteFromGivenNode("Sanctuary by Joji", head);
+	traverse(head);
+	
+	return 0;
     }
     
-    
-    
-    
+   
     
     
     
