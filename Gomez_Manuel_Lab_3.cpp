09d50dab644af4c39b6a2e31ce182c66bc7612b9
Manuel Gomez <24-16819@g.batstate-u.edu.ph>
2025-04-26 19:39:34 +0800

    Sorry po late

---
+++ Gomez_Manuel_Lab_3.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+char stack[30];
+int top = -1;
+
+string stackOutput = "";
+
+void push(char val){
+    stack[++top] = val;
+    stackOutput += val;
+}
+char pop(){
+    stackOutput.erase(top, 1);
+    char val = stack[top--];
+    return val;
+}
+char peek(){
+    return stack[top];
+}
+int checkLevel(char operation){
+    int level;
+    
+    if(operation == '(' || operation == ')'){
+        level = 0;
+    }else if(operation == '^'){
+        level = 1;
+    }else if(operation == '/' || operation == '*'){
+        level = 2;
+    }else if(operation == '+' || operation == '-'){
+        level = 3;
+    }else{
+        level = 0;
+    }
+    
+    return level;
+}
+void convertToPostfix(string input){
+    string output = "";
+
+    cout << "| Input | Stack | Output |" << endl;
+    cout << "|------------------------|" << endl;
+    
+    for (int i = 0; i < input.length(); i++){
+        if(isdigit(input[i])){
+            output += input[i];
+        } else {
+            if(top == -1){
+                push(input[i]);
+            } else {
+                if(input[i] == '(' || peek() == '('){
+                    push(input[i]);
+                } else {
+                    if(input[i] == ')'){
+                        while(top >= 0 && peek() != '('){
+                            output += pop();
+                        }
+                             if(top >= 0 && peek() == '('){
+                            pop();
+                             } else {
+                             while(checkLevel(peek()) <= checkLevel(input[i])){
+                            output += pop();
+                        }
+                        push(input[i]);
+                        }
+                    }
+                }
+            }
+        }
+        cout << "|   " << input[i] << "  |  " << stackOutput << "  | " << output << "  |  " << endl;
+        cout << "|------------------------|" <<endl;
+    }
+        while(top >= 0){
+            output += pop();
+    }
+    cout <<"The equivalent postfix is: " << output << endl;
+}
+
+int main (){
+    string inputA = "(2+3)-5*4/5";
+    convertToPostfix(inputA);
+    
+    cout << endl;
+    
+    string inputB = "(5+5)*(8-2)";
+    convertToPostfix(inputB);
+    
+    cout << endl;
+    
+    string inputC = "5+5+3*4-7";
+    convertToPostfix(inputC);
+    
+    cout << endl;
+    
+    string inputD = "(5+6)*2+1";
+    convertToPostfix(inputD);
+    
+    cout << endl;
+    
+    string inputE = "(5+6)*(2+1)";
+    convertToPostfix(inputE);
+    
+    return 0;
+}
